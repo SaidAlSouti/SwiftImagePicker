409a603c45caa8ca18eddb9e3f8ad6eab4c6e5e4
Personal <Personal@omans-iMac.local>
2019-09-04 13:42:48 +0400

    initial commit

--- ImagePicker/ViewController.swift
+++ ImagePicker/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
-
+import YPImagePicker
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageViewer: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func PickImage(_ sender: Any) {
+        // https://github.com/Yummypets/YPImagePicker
+        var config = YPImagePickerConfiguration()
+        config.showsFilters = false
+        config.onlySquareImagesFromCamera = false
+        config.startOnScreen = .library
+        
+        config.targetImageSize = .cappedTo(size: 1280)
+        
+        
+    
+//        config.showsCrop = .rectangle(ratio: 2)
+        let picker = YPImagePicker(configuration: config)
+        picker.didFinishPicking { [unowned picker] items, _ in
+            if let photo = items.singlePhoto {
+                print(photo.fromCamera) // Image source (camera or library)
+                print(photo.image) // Final image selected by the user
+                print(photo.originalImage) // original image selected by the user, unfiltered
+                print(photo.modifiedImage) // Transformed image, can be nil
+                print(photo.exifMeta) // Print exif meta data of original image.
+                
+                self.imageViewer.image = photo.image
+            }
+            picker.dismiss(animated: true, completion: nil)
+        }
+        present(picker, animated: true, completion: nil)
+    }
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ImagePicker' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  pod 'YPImagePicker', '~> 3.5.2'
+  use_frameworks!
+
+  # Pods for ImagePicker
+
+  target 'ImagePickerTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ImagePickerUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
